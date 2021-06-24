7b33136a22d5b46150bcdd22f7a29cc073dc32e2
mohamednafa <86365035+mohamednafa@users.noreply.github.com>
2021-06-24 07:32:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,49 +1,21 @@
-"use strict"
 
-if ("serviceWorker" in navigator) {
-  // Delay registration until after the page has loaded, to ensure that our
-  // precaching requests don't degrade the first visit experience.
-  // See https://developers.google.com/web/fundamentals/instant-and-offline/service-worker/registration
-  window.addEventListener("load", function() {
-    // Your service-worker.js *must* be located at the top-level directory relative to your site.
-    // It won't be able to control pages unless it's located at the same level or higher than them.
-    // *Don't* register service worker file in, e.g., a scripts/ sub-directory!
-    // See https://github.com/slightlyoff/ServiceWorker/issues/468
-    navigator.serviceWorker
-      .register("https://www.tqreer70.com/sw.js")
-      .then(function(reg) {
-        // updatefound is fired if service-worker.js changes.
-        reg.onupdatefound = function() {
-          // The updatefound event implies that reg.installing is set; see
-          // https://w3c.github.io/ServiceWorker/#service-worker-registration-updatefound-event
-          var installingWorker = reg.installing
+const js = `
+'use strict';
+const CACHE_VERSION=1;
+let CURRENT_CACHES={offline:"offline-v1"};
+const OFFLINE_URL="https://www.tqreer70.com/p/offline.html";
+function createCacheBustedRequest(a){let b=new Request(a,{cache:"reload"});if("cache"in b)return b;let c=new URL(a,self.location.href);return c.search+=(c.search?"&":"")+"cachebust="+Date.now(),new Request(c)}self.addEventListener("install",a=>{a.waitUntil(fetch(createCacheBustedRequest(OFFLINE_URL)).then(function(a){return caches.open(CURRENT_CACHES.offline).then(function(b){return b.put(OFFLINE_URL,a)})}))}),self.addEventListener("activate",a=>{let b=Object.keys(CURRENT_CACHES).map(function(a){return CURRENT_CACHES[a]});a.waitUntil(caches.keys().then(a=>Promise.all(a.map(a=>{if(-1===b.indexOf(a))return console.log("Deleting out of date cache:",a),caches.delete(a)}))))}),self.addEventListener("fetch",a=>{("navigate"===a.request.mode||"GET"===a.request.method&&a.request.headers.get("accept").includes("text/html"))&&(console.log("Handling fetch event for",a.request.url),a.respondWith(fetch(a.request).catch(a=>(console.log("Fetch failed; returning offline page instead.",a),caches.match(OFFLINE_URL)))))});
 
-          installingWorker.onstatechange = function() {
-            switch (installingWorker.state) {
-              case "installed":
-                if (navigator.serviceWorker.controller) {
-                  // At this point, the old content will have been purged and the fresh content will
-                  // have been added to the cache.
-                  // It's the perfect time to display a "New content is available; please refresh."
-                  // message in the page's interface.
-                  console.log("New or updated content is now available.")
-                  document.querySelector('#update-message').classList.add('show')
-                } else {
-                  // At this point, everything has been precached.
-                  // It's the perfect time to display a "Content is cached for offline use." message.
-                  console.log("Content is now available offline!")
-                }
-                break
+`
 
-              case "redundant":
-                console.error("The installing service worker became redundant.")
-                break
-            }
-          }
-        }
-      })
-      .catch(function(e) {
-        console.error("Error during service worker registration:", e)
-      })
+async function handleRequest(request) {
+  return new Response(js, {
+    headers: {
+      "content-type": "application/javascript;charset=UTF-8",
+    },
   })
 }
+
+addEventListener("fetch", event => {
+  return event.respondWith(handleRequest(event.request))
+})
